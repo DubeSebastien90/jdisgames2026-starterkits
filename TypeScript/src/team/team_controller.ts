0399e1f74837a80da8386493952cf999768915b8
DubeSebastien90 <sebastien.dube.10@ulaval.ca>
2026-07-25 14:12:13 -0400

    lol

--- TypeScript/src/team/team_controller.ts
+++ TypeScript/src/team/team_controller.ts
@@ -33,7 +33,7 @@ type BehaviourFactory = () => IBehaviour;
  */
 export class TeamController {
   private static readonly ROSTER: Record<string, BehaviourFactory> = {
-    BotA: () => new GathererBehaviour(),
+    BotA: () => new CompanionFarmerBehaviour(),
     BotB: () => new GathererBehaviour(),
   };
 
